65cbf12d14ba13b934ee32ec7f4521c3523dde18
Ermy99 <eizihirwe@gmail.com>
2018-10-20 22:15:21 -0400

    adding and updating

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template
+from flask_heroku import Heroku
+from flask_sqlalchemy import SQLAlchemy
+import sys
+import json
+
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
 @app.route("/")
 def main():
     return render_template("IndexPage.html")
